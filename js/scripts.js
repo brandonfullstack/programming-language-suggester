e9ffc711e83d404ce9729a525bd0e141236beeb6
Brandon Wright <brandonfullstack@gmail.com>
2023-06-17 15:11:01 -0400

    Add form logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,66 @@
-// Date value
-const startDate = document.getElementById("startDate").value;
+// User Interface Logic
 
-// Color value
-const phoneColor = document.getElementById("phoneColor").value;
-
-// Input value
-const favoriteNumber = document.getElementById("favoriteNumber").value;
-
-// Select boxes value
-function handleSelect(event) {
-  event.preventDefault();
-  const animalPetted = document.getElementById("animalPetted").value;
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("swift").setAttribute("class", "hidden");
 }
 
-window.addEventListener("load", function() {
-  document.getElementById("select-form").addEventListener("submit", handleSelect);
-});
-
-// Radio button value
-function handleRadio(event) {
-  event.preventDefault();
-  const radioSelection = document.querySelector("input[name='flavor']:checked").value;
+function hexToNumber(color) {
+  const colorNumberString = color.substr(4, 1);
+  return parseInt(colorNumberString, 16);
 }
 
-window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", handleRadio);
-});
+function dateToNumber(date) {
+  const dateNumberString = date.substr(5);
+  return parseInt(dateNumberString, 16);
+}
+
+window.onload = function() {
+  const form = document.querySelector("form");
+  
+  function resetForm() {
+    form.reset();
+    document.getElementById("error-message").classList.add("hidden");
+  }
+
+  document.getElementById("resetFormButton").addEventListener("click", (event) => {
+    resetForm();
+    console.log("anything");
+  })
+  hideResultsAndError();
+
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    hideResultsAndError();
+    const startDateNumber = dateToNumber(document.getElementById("startDate").value);
+    const phoneColorNumber = hexToNumber(document.getElementById("phoneColor").value);
+    const favoriteNumber = parseInt(document.getElementById("favoriteNumber").value);
+    const animalPettedNumber = parseInt(document.getElementById("animalPetted").value);
+    const surveySelectionNumber = parseInt(document.querySelector("input[name='survey']:checked")?.value);
+    
+// console.log(startDateNumber + "\n" + phoneColorNumber + "\n" + favoriteNumber + "\n" + animalPettedNumber + "\n" + surveySelectionNumber);
+
+const totalNumber = startDateNumber + phoneColorNumber + favoriteNumber + animalPettedNumber + surveySelectionNumber;
+
+    if (!startDateNumber || !phoneColorNumber || !favoriteNumber || !animalPettedNumber || !surveySelectionNumber) {
+      document.getElementById("error-message").removeAttribute("class");
+    } else { 
+
+      if (totalNumber > 24) {
+      document.getElementById("python").removeAttribute("class");
+    
+      } else if (totalNumber < 12) {
+      document.getElementById("ruby").removeAttribute("class");
+      
+    } else if (totalNumber >= 12 && totalNumber <= 24) {
+      document.getElementById("swift").removeAttribute("class");
+
+      } else {
+      document.getElementById("sorry").removeAttribute("class");
+      }
+    }
+    form.reset();
+  };
+};
